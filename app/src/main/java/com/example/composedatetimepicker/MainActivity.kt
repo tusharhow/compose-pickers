65a55e6e46b8d1d5b9b84face68e064ebc3fe151
tusharhow <tusharsoftdev@gmail.com>
2023-01-04 21:58:44 -0800

    DONE

--- app/src/main/java/com/example/composedatetimepicker/MainActivity.kt
+++ app/src/main/java/com/example/composedatetimepicker/MainActivity.kt
@@ -1,19 +1,19 @@
 package com.example.composedatetimepicker
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.composedatetimepicker.screens.SelectionScreen
 import com.example.composedatetimepicker.ui.theme.ComposeDateTimePickerTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
